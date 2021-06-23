d7dea3461b0de573742ebe17d8c5d08c32188b29
sammy <sammyoina@gmail.com>
2021-06-24 00:34:37 +0300

    spectrum analyzer

--- client.py
+++ client.py
@@ -1,4 +1,8 @@
 import socket
+from spectrum import SpectrumAnalyzer
+
+#create spectrum analyzer
+spec = SpectrumAnalyzer()
 
 def get_ips_for_host(host):
     try:
@@ -14,4 +18,20 @@ message = "23,2,3,4"
 
 clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-clientSock.sendto(str.encode(message), (UDP_IP_ADDRESS, UDP_PORT_NO))
+try:
+    while True:
+        spec.data = spec.audioinput()
+        spec.fft()
+        spec.graphplot()
+        print("sub bass: ", max(spec.spec_y[0:2]))
+        print("bass: ", max(spec.spec_y[3:8]))
+        print("low mid: ", max(spec.spec_y[9:16]))
+        print("mid: ", max(spec.spec_y[17:64]))
+        print("upper mid: ", max(spec.spec_y[65:128]))
+        print("presence: ", max(spec.spec_y[129:192]))
+        print("brilliance:", max(spec.spec_y[193:255]))
+
+        clientSock.sendto(str.encode(message), (UDP_IP_ADDRESS, UDP_PORT_NO))
+except KeyboardInterrupt:
+    spec.pa.close()
+    print("End...")
